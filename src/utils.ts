b2de3fa96ec6c4955ca8c980b64d1d1296faf11c
李武周 <289901948@qq.com>
2021-12-11 22:56:31 +0800

    Add `Stack` class

--- src/utils.ts
+++ src/utils.ts
@@ -23,3 +23,50 @@ export const isSymbol = (val: unknown): val is symbol =>
 	typeof val === "symbol";
 
 export const isArray = Array.isArray;
+
+export class StackNode<T> {
+	constructor(public value: T, public next: StackNode<T> | null = null) {}
+	public toString(): string {
+		return `value: ${this.value},next: ${this.next}`;
+	}
+}
+
+export class Stack<T> {
+	private _head: StackNode<T> | null = null;
+
+	private _size: number = 0;
+
+	public peek(): T | null {
+		if (this.isEmpty()) {
+			return null;
+		}
+		return this._head!.value;
+	}
+	public push(value: T): void {
+		const node = new StackNode<T>(value);
+		if (this.isEmpty()) {
+			this._head = node;
+		} else {
+			node.next = this._head!;
+			this._head = node;
+		}
+		this._size++;
+	}
+
+	public pop(): T | null {
+		if (this.isEmpty()) {
+			return null;
+		}
+		const node = this._head;
+		this._head = node!.next;
+		return node!.value;
+	}
+
+	public isEmpty(): boolean {
+		return this.size() === 0;
+	}
+
+	public size(): number {
+		return this._size;
+	}
+}
