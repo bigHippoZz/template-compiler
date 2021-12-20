0a89f9ce688445546ea31ef1ee1234adae00403f
李武周 <289901948@qq.com>
2021-12-20 13:39:43 +0800

    Support more symbols

--- src/CharCodes.ts
+++ src/CharCodes.ts
@@ -9,20 +9,28 @@ export enum CharCodes {
 
 	Tab = 9, // "\t"
 	NewLine = 10, // "\n"
+	Vtab = 13, // 垂直制表符 \v
 	FormFeed = 12, // "\f"
 	CarriageReturn = 13, // "\r"
 	Space = 32, // " "
 
 	ExclamationMark = 33, // "!"
-	Dash = 45, // "-"
-	Slash = 47, // "/"
-	Questionmark = 63, // "?"
+
+	QuestionMark = 63, // "?"
 
 	Zero = 48, // "0"
 	Nine = 57, // "9"
 
 	UpperA = 0x41, // "A"
 	LowerA = 0x61, // "a"
+	LowerE = 101, // "e"
+	UpperE = 69, // "E"
+	LowerU = 117, // "u"
+	LowerN = 110, // "n"
+	LowerR = 114, // "r"
+	LowerT = 116, // "t"
+	LowerV = 118, // "v"
+
 	UpperF = 0x46, // "F"
 	LowerF = 0x66, // "f"
 	UpperZ = 0x5a, // "Z"
@@ -32,6 +40,37 @@ export enum CharCodes {
 	Brackets = 91, // '['
 
 	EOF = 0, // EOF
+
+	Underscore = 95, // _
+	Dollar = 36, // $
+
+	LPAREN = 40, // (
+	RPAREN = 41, // )
+
+	LBRACE = 123, // {
+	RBRACE = 125, // }
+
+	LBRACKET = 91, // [
+	RBRACKET = 93, // ]
+
+	COMMA = 44, // ,
+	COLON = 58, // :
+	SEMICOLON = 59, // ;
+	Period = 46, // .
+
+	HASH = 35, // #
+
+	Plus = 43, // +
+	Minus = 45, // "-"
+	Star = 42, // "-"
+	Slash = 47, // "/"
+	Percent = 37, // %
+	Caret = 94, // ^
+	Ampersand = 38, // &
+
+	Bar = 124, // |
+	Nbsp = 160, // &nbsp
+	Backslash = 92, // \
 }
 
 /**
@@ -80,9 +119,7 @@ export function isQuote(code: number) {
 }
 
 export function isPrefixEnd(code: number): boolean {
-	return (
-		isAsciiLetter(code) && code >= CharCodes.Zero && code <= CharCodes.Nine
-	);
+	return isAsciiLetter(code) && code >= CharCodes.Zero && code <= CharCodes.Nine;
 }
 
 export function isNameEnd(code: number): boolean {
