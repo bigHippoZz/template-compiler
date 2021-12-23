3729da8b58f84e0922e8a5f076494bcd84825644
李武周 <289901948@qq.com>
2021-12-23 11:56:32 +0800

    Update cli

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,34 @@ import resolve from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
 
-const __DEV__ = process.env.NODE_ENV === 'production';
+const __DEV__ = process.env.NODE_ENV !== 'production';
 
 const extensions = ['.js', '.ts'];
-export default {
-	input: 'src/main.ts',
-	output: { file: 'dist/template.js', sourcemap: true, format: 'es', indent: false },
-	plugins: [
-		resolve({
-			extensions,
-		}),
-		commonjs(),
-		babel({
-			extensions,
-			exclude: 'node_modules/**',
-			skipPreflightCheck: true,
-			babelHelpers: 'bundled',
-		}),
-		livereload(),
+
+const plugins = [
+	resolve({
+		extensions,
+	}),
+	commonjs(),
+	babel({
+		extensions,
+		exclude: 'node_modules/**',
+		skipPreflightCheck: true,
+		babelHelpers: 'bundled',
+	}),
+	__DEV__ && livereload(),
+	__DEV__ &&
 		serve({
 			open: true,
 			port: 8888,
 		}),
-	],
+	!__DEV__ && terser(),
+];
+
+export default {
+	input: 'src/main.ts',
+	output: { file: 'dist/template.js', sourcemap: true, format: 'es', indent: false },
+	plugins,
 };
